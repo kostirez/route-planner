1bcf961338bb7c2eeea1d2aeb2a7be57e6701888
Martin Kostohryz <martin.kostohryz@gmail.com>
2021-03-18 21:31:52 +0100

    creating sideMenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OlMapComponent } from './map/ol-map/ol-map.component';
+import {SideMenuComponent} from './map/side-menu/side-menu.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     OlMapComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
